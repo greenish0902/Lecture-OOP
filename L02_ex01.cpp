d8ac785fc22b71d275c2b2850b3098ce6e60f743
greenish0902 <greenish0902@gmail.com>
2021-09-23 23:40:25 +0900

    file name changed

--- L02_ex01.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-  int value1, value2, sum;
-  cout << "Please enter two integer values: ";
-  cin >> value1 >> value2;
-  sum = value1 + value2;
-  cout << value1 << " + " << value2 << " = " << sum << '\n';
-}
